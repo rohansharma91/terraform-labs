3cdc8755b67668507981559225e4a4d90ec48e21
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-10 17:28:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,10 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  for_each = toset(["one", "two", "three"])
-
-  name = "instance-${each.key}"
+  name = "single-instance"
 
   instance_type          = "t2.micro"
   
-  monitoring             = false
- 
-
   tags = {
     Terraform   = "true"
     Environment = "dev"
