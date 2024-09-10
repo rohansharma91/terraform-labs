827217c517e7486b3a561b611809944ebba68dd9
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-10 15:05:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test" {
   ami           = "ami-0182f373e66f89c85"
-  instance_type = "t2.micro"
+  instance_type = var.intance_type
 
   tags = {
     Name = "Terraform instace"
