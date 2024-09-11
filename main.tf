7b6a6d5f109bae6054463af0fdd4198a87eaf323
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-11 15:31:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket_ro19912"
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "example_bucket_ro19912"
+}
+tags {
+  Env= dev
+  } 
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.example.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
