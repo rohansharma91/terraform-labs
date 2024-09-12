79f5e35335b656071b16ce73e307f1430ab835cb
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-12 14:43:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+resource "aws_instace" "test"{
+  ami= ""
+  instance_type= "t2.micro"
 
+tags= {
+   Name= terraform test
+
+ }
+}
+
+resource "aws_vpc" "test_vpc"{
+  name= "my-vpc"
+
+  cidr = "10.0.0.0/16"
