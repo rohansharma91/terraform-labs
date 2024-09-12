72581cd3870d6ad75d8e95179f3f683e47b421d1
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-12 14:52:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instace" "test"{
   ami= ""
   instance_type= "t2.micro"
+  subnet_id= aws_subnet.test_subnet.id
 
 tags= {
    Name= terraform test
@@ -12,3 +13,12 @@ resource "aws_vpc" "test_vpc"{
   name= "my-vpc"
 
   cidr = "10.0.0.0/16"
+
+}
+
+resource "aws_subnet" "test_subnet"{
+  vpc_id= aws_vpc.test_vpc.id
+  cidr_block = "10.0.128.0/17"
+
+ }
+}
